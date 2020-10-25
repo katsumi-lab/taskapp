59a6c249ccbec8b3022e50f3c068ecee425bcc1b
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-25 21:21:35 +0900

    ユーザー管理権限機能

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   end
 
   def show
-  @task = Task.find(params[:id])
+  @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -21,16 +21,16 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
   def update
-    task = Task.find(params[:id])
+    task = @task = current_user.tasks.find(params[:id])
     task.update(task_params)
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました"
   end
 
   def destroy
-    task = Task.find(params[:id])
+    task = @task = current_user.tasks.find(params[:id])
     task.destroy
     redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
